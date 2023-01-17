33ac6eeac48c70c4259b71c7e3ab224f34463077
MekhiHart <mekhihartdelacruz@gmail.com>
2023-01-16 21:49:35 -0800

    Render checkboxes from category picked by user

--- src/Components/Body Component/AvailableClasses.js
+++ src/Components/Body Component/AvailableClasses.js
@@ -1,10 +1,42 @@
 export default function AvailableClasses(props){
+    // classObj = {valueName:string, availableClasses:[]}
+    //classObj.availableClasses = {className}
     const {selectedClasses} = props.formData
-    const test = selectedClasses.map(x => <h2 key={x.valueName}>{x.valueName}</h2>)
+    const divClasses = selectedClasses.map(classObj => {
+        const divTitle = classObj.valueName
+        const classesCheckbox = classObj.availableClasses.map(availableClass => (
+            <label key={availableClass.className} htmlFor={availableClass.className}>
+                <input 
+                type="checkbox"
+                name="availableClasses"
+                value={availableClass.className}
+                id={availableClass.className}
+                >
+                </input>
+                {availableClass.className}
+            </label>
+        ))
+        
+        console.log("Obj; ",classesCheckbox)
+
+
+        return(
+            <div className="checkBox--div scroll">
+                <h3>{divTitle}</h3>
+                {classesCheckbox}
+            </div>
+
+        )
+    })
+
+
 
     console.log(selectedClasses)
 
     return(
-        test
+        <form>
+            <h2>Available Classes</h2>
+            {divClasses}
+        </form>
     )
 }
